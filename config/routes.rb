1694909a034f6c18fe29d72d7be95bba12cc09b1
akira matsushima <sp08138am@gmail.com>
2019-06-20 17:43:27 +0900

    画像アップローダー機能作成済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new,:show, :create,]
+  resources :feeds
 end
