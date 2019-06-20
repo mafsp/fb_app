c10c464ec8655eb333a3e599581c5bea02ebc460
akira matsushima <sp08138am@gmail.com>
2019-06-20 15:48:09 +0900

    詳細ページ作成済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
-  get '/users/new', to:'users#new'
-  post '/users', to:'users#create'
+  resources :users, only: [:new,:show, :create, :destroy]
 end
