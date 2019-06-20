189f16a59d450e0670366d02c067eccfcfca51ab
akira matsushima <sp08138am@gmail.com>
2019-06-20 16:16:59 +0900

    セッション作成開始。ログインに失敗した場合の処理を実装済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
-  resources :users, only: [:new,:show, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new,:show, :create,]
 end
